d5b781b490e568d7dcd9d5bda650ac5121cfcea7
dhirajj14 <djain14@hawk.iit.edu>
2020-07-04 14:00:32 -0500

    complete layout challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,11 +12,40 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Layout Challenge',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              color: Colors.red,
+              width: 100,
+            ),
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    color: Colors.yellow,
+                    height: 100,
+                    width: 100,
+                  ),
+                  Container(
+                    color: Colors.green,
+                    height: 100,
+                    width: 100,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              width: 100,
+            ),
+          ],
+        )),
       ),
-      home: Scaffold(),
     );
   }
 }
